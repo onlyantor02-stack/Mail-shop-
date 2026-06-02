3f7fceecd9b36813f74e9ee23b3fd451b1c1e71b
onlyantor02-stack <onlyantor02@gmail.com>
2026-06-03 03:13:03 +0600

    Add HTTP server to respond with bot status

--- index.js
+++ index.js
@@ -148,3 +148,14 @@ bot.onText(
 );
 
 console.log("Bot Running...");
+const http = require("http");
+
+const PORT = process.env.PORT || 3000;
+
+http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("Bot is running");
+}).listen(PORT, () => {
+  console.log("Server running on port " + PORT);
+});
+t
